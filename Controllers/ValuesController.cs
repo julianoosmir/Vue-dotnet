9eae14990609a0df80431b83c78440e94bc3de87
julianoosmir <60453624+julianoosmir@users.noreply.github.com>
2020-08-30 20:30:02 -0400

    gitignore file

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VueDemo.Controllers
+{
+   
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ValuesController : ControllerBase
+         {
+         [HttpGet]
+         public IActionResult Get()
+         {
+            return Ok(new string[] { "value1", "value2" });
+         }
+        }
+    
+}
---
+++ cliente/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div :key="r" v-for="r in this.results">{{ r }}</div>
+    </div>
+</template>
+<script>
+export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                results: []
+            };
+        },
+        async created() {
+            const r = await fetch('/api/values');
+            this.results = await r.json();
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
